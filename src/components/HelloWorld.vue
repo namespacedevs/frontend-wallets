bae06e5f477cbda12826d386e2152eaafced65c9
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-03-04 17:49:58 -0300

    change classes to match tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="container mx-auto text-center">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
 </template>

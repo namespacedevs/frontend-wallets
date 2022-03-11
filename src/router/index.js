310ed20cd35e5bb0201e86551959b939a332b96a
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-03-11 11:21:06 -0300

    create router const with home route

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppLayout from "../layouts/AppLayout.vue";
+import HelloWorld from "../components/HelloWorld.vue";
+
+export const router  = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: AppLayout,
+            children: [{
+                path: '/hello',
+                component: HelloWorld
+            },
+        }
+    ]
+})

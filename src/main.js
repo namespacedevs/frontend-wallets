db442280b7d8d52f70b5cc2fd90c2bfcbd347d5d
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-03-11 11:19:23 -0300

    import router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { router } from './router/index'
 import './index.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App).use(router)
+app.mount('#app')
